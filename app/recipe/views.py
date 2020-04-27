c3c8fa916db11a855d640575386f765942f5e08c
Thomas Stokes <tstokes661@gmail.com>
2020-04-27 14:23:51 -0700

    Tag and Ingredient Updates

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet,
+                    mixins.ListModelMixin,
+                    mixins.CreateModelMixin):
+    """mangane tags in the db"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """return objects for auth user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serailizer):
+        """Create a new tag"""
+        serailizer.save(user=self.request.user)
