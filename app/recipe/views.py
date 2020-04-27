899ad4adb7d6d0a2e575e6e6c7c089ab2238b1fb
Thomas Stokes <tstokes661@gmail.com>
2020-04-27 15:19:57 -0700

    Added Ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,13 +2,13 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
 
 
 class TagViewSet(viewsets.GenericViewSet,
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
+mixins.ListModelMixin,
+mixins.CreateModelMixin):
     """mangane tags in the db"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -22,3 +22,21 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serailizer):
         """Create a new tag"""
         serailizer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+mixins.ListModelMixin,
+mixins.CreateModelMixin):
+    """manage ingreidents in the db"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """return objects for the current auth user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """create a new ingredient"""
+        serializer.save(user=self.request.user)
